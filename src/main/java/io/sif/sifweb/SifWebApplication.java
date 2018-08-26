5e59d250d6dbdd28e9fdcba81ce1faa8f93f6402
linxinzhe <linxinzhechn@qq.com>
2018-08-26 18:26:20 +0800

    Initial commit

---
+++ src/main/java/io/sif/sifweb/SifWebApplication.java
@@ -0,0 +1,12 @@
+package io.sif.sifweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SifWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SifWebApplication.class, args);
+    }
+}
---
+++ src/test/java/io/sif/sifweb/SifWebApplicationTests.java
@@ -0,0 +1,16 @@
+package io.sif.sifweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SifWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
